d7e8f34ecce5706d82007f638fcaf1f94b08d325
Shin-sibainu <shincode0712@gmail.com>
2022-02-16 19:37:46 +0900

    make it better

--- app.js
+++ app.js
@@ -2,11 +2,9 @@ const express = require("express");
 const app = express();
 const taskRoute = require("./routes/tasks");
 const connectDB = require("./db/connect");
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
+require("dotenv").config();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 //ミドルウェア
 app.use(express.static("./public")); //これでHTMLとか読み込める
@@ -17,7 +15,7 @@ app.use("/api/v1/tasks", taskRoute);
 const start = async () => {
   try {
     await connectDB(process.env.DEV_MONGO_URL || process.env.MONGODB_URI);
-    app.listen(PORT || process.env.PORT, console.log(`サーバーが起動しました`));
+    app.listen(PORT, console.log(`サーバーが起動しました`));
   } catch (error) {
     console.log(error);
   }
